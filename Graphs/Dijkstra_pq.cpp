1445f5f1f50fba447e5c5688dd5640218d836be8
Aditya Anand <anand.aditya0803@gmail.com>
2020-12-24 01:40:50 +0530

    Adding code for Graphs

--- Graphs/Dijkstra_pq.cpp
+++ Graphs/Dijkstra_pq.cpp
@@ -28,14 +28,49 @@ public:
             }
             visited[i] = false;
         }
-        int vtx = 0;
-        pq.push(make_pair(distance[vtx], vtx));
-        for()    
+        int src = 0;
+        pq.push(make_pair(distance[src], src));
+        while(!pq.empty()){
+            pair<int, int> top = pq.top();
+            pq.pop();
+            if(visited[top.second] == true){
+                continue;
+            }
+            visited[top.second] = true;
+             
+            for(auto neighbour : gmap[top.second]){
+                int child = neighbour.first;
+                int child_from_vtx = neighbour.second;
+                int child_from_src = top.first + neighbour.second;
+                
+                if(child_from_src < distance[child]){
+                    distance[child] = child_from_src;
+                    pq.push(make_pair(distance[child], child));
+                }  
+            }
+        }
+        for(int i = 0; i < V; i++){
+            if(distance[i] == INT_MAX){
+                 cout << "Vertex: " << i << " ==> " << "-1" << '\n';   
+            }
+            else{
+               cout << "Vertex: " << i << " ==> " << distance[i] << '\n';
+            }
+        } 
     }
 };
 
 int main(){
-    std::ios_base::sync_with_stdio(false);
+    // * break the sync between c++ and c I/O functions --> for faster input, use it (Generally when input file is very large)
+    std::ios_base::sync_with_stdio(false); 
+    // * untie cin and cout (Not important as such)
     cin.tie(NULL);
-
+    Graph g;
+    int V, E, u, v, d;
+    cin >> V >> E;
+    for(int i = 0; i < E; i++){
+        cin >> u >> v >> d;
+        g.addEdge(u, v, d);
+    }
+    g.dijkstra(V, E, g);
 }
