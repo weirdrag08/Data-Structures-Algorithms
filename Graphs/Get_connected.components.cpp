d119f2c24d97c10e6a123b010c41b48133f14fae
Aditya Anand <anand.aditya0803@gmail.com>
2020-12-30 23:43:58 +0530

    Adding codes for Graphs

--- Graphs/Get_connected.components.cpp
+++ Graphs/Get_connected.components.cpp
@@ -13,65 +13,71 @@ using namespace std;
 #define INFI 10e8
 #define INF 10e7
 
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-typedef vector<bool> vb;
+typedef pair < int, int > pi;
+typedef vector < int > vi;
+typedef vector < bool > vb;
 
-class Graph{
-public:
-    unordered_map<int, list<pi> > gmap;
+class Graph {
+    public:
+        unordered_map < int, list < pi > > gmap;
 
-    void addEdge(int src, int dest, int weight, bool bidir = true){
+    void addEdge(int src, int dest, int weight, bool bidir = true) {
         gmap[src].push_back(mp(dest, weight));
-        if(bidir){
+        if (bidir) {
             gmap[dest].push_back(mp(src, weight));
         }
     }
-private:
-    vi* get_connected_components_helper(int src, vb &visited){
-        visited[src] = true;
-        vi *connected_components = new vi();
-        for(auto neighbour : gmap[src]){
-            if(!visited[neighbour.first]){
-                visited[neighbour.first] = true;
-                connected_components = get_connected_components_helper(neighbour.first, visited);
-            }
-        }
-        connected_components->push_back(src);
-        return connected_components;
-    }
-public: 
-    void get_connected_components(int V, int E, Graph g){
-        vb visited(V, false);
-        vi *output;
-        cout << "[";
-        int i = 0;
-        for(; i < V; i++){
-            if(!visited[i]){
-                output = get_connected_components_helper(i, visited);
-                cout << "[";
-                for(int j = output-> size() - 1; j >= 0; j--){
-                    if(j != 0){
-                        cout << output->at(j) << ", ";
-                    }
-                    else{
-                        cout << output-> at(j);
-                    }
+    private:
+        vi * get_connected_components_helper(int src, vb & visited) {
+            visited[src] = true;
+            vi * connected_components = new vi();
+            for (auto neighbour: gmap[src]) {
+                if (!visited[neighbour.first]) {
+                    visited[neighbour.first] = true;
+                    connected_components = get_connected_components_helper(neighbour.first, visited);
                 }
-              
             }
+            connected_components -> push_back(src);
+            return connected_components;
         }
-        
-        cout << "]" << '\n';
-    }
+    public:
+        void get_connected_components(int V, int E, Graph g) {
+            vb visited(V, false);
+            vi * output;
+            cout << "[";
+            int i = 0;
+            int count=0;
+            for (; i < V; i++) {
+               
+                if (!visited[i]) {
+                    count++;
+                    output = get_connected_components_helper(i, visited);
+                    if(count>1){
+                        cout<<", ";
+                    }
+                    cout << "[";
+                    for (int j = output -> size() - 1; j >= 0; j--) {
+                        if (j > 0) {
+                            cout << output -> at(j) << ", ";
+                        } else {
+                            cout << output -> at(j);
+                        }
+                    }
+                    cout << "]";
+                    
+                }
+            }
+
+            cout << "]" << '\n';
+        }
 };
 
-int main(){
+int main() {
     std::ios_base::sync_with_stdio(false);
     Graph g;
     int V, E, u, v, w;
     cin >> V >> E;
-    for(int i = 0; i < E; i++){
+    for (int i = 0; i < E; i++) {
         cin >> u >> v >> w;
         g.addEdge(u, v, w);
     }
