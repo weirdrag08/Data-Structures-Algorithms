bdf66b9d77ae42dd097fd93f4b4cc73bc336662b
Aditya Anand <anand.aditya0803@gmail.com>
2021-01-12 14:36:03 +0530

    Adding codes for Hashmaps and Heaps

--- Hashmaps_and_heaps/Iternary_tickets.cpp
+++ Hashmaps_and_heaps/Iternary_tickets.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-#include<unordered_map>
-#include<list>
-#include<vector>
-#include<climits>
-#include<algorithm>
-#include<stack>
-#include<queue>
-#include<string>
-#include<utility>
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <climits>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <string>
+#include <utility>
 using namespace std;
 
 #define mp make_pair
@@ -18,32 +18,39 @@ typedef pair<int, int> pi;
 typedef vector<int> vi;
 typedef vector<bool> vb;
 
-int main(){
+int main()
+{
     unordered_map<string, string> start, route;
     int n;
     cin >> n;
     string src, dest;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> src >> dest;
         route[src] = dest;
         start[src] = dest;
     }
-   
-   for(auto travel : route){
-       if(start.count(travel.second) > 0){
-           start.erase(travel.second);
-       }
-   }
- 
-   unordered_map<string, string>::iterator it = start.begin();
-    while(!route.empty()){
-        cout << it-> first << " -> " << it-> second;
-        auto new_it = route.find(it-> second);
-        if(new_it == route.end()){
+
+    for (auto travel : route)
+    {
+        if (start.count(travel.second) > 0)
+        {
+            start.erase(travel.second);
+        }
+    }
+
+    unordered_map<string, string>::iterator it = start.begin();
+    cout << it->first;
+    while (!route.empty())
+    {
+        cout << " -> " << it->second;
+        auto new_it = route.find(it->second);
+        if (new_it == route.end())
+        {
             break;
         }
-        route.erase(it);
+        route.erase(it->first);
         it = new_it;
     }
-   cout << '\n';
+    cout << "." << '\n';
 }
