64d1010b7b3ce5787162fae71766290f71dea1ca
Aditya Anand <anand.aditya0803@gmail.com>
2021-03-04 23:46:44 +0530

    Adding codes for DP

---
+++ Dynamic_programming/Tiling_2*1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define INFI 10e8
+#define INF 10e7
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie();
+}
