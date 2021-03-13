61e6afd29c600ffc8996b1690fff2c37fe5a0460
Aditya Anand <anand.aditya0803@gmail.com>
2021-03-13 23:52:31 +0530

    Adding codes for DP

---
+++ Dynamic_programming/Buy_and_sell_stocks_with_2_transactions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie();
+    int n, _max = INT_MIN, _max2 = INT_MIN, _min = INT_MAX;
+    cin >> n;
+    vi v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    for(int i = 0; i < n; i++){
+       
+    }
+}
---
+++ Dynamic_programming/Buy_and_sell_stocks_with_fee.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie();
+
+    int n, fee;
+    cin >> n;
+    vi v(n);
+    vvi dp(n, vi(2, 0));
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    cin >> fee;
+    
+    dp[0][0] -= v[0];
+    dp[0][1] = 0;
+
+    for(int i = 1; i < n; i++){
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - v[i]);
+        dp[i][1] = max(dp[i - 1][1], v[i] + dp[i - 1][0] - fee);
+    }
+    cout << dp[n - 1][1] << '\n';
+}
