8cd03a83338cf76989282331cbf440b090145b53
Aditya Anand <anand.aditya0803@gmail.com>
2021-03-19 23:24:34 +0530

    Adding codes for DP

---
+++ Dynamic_programming/Kadane_algorithm_empty.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie();
+
+    // * If we can return a empty set, then the minimal value which can be returned is 0, because ∑(empty) = 0, ie if max subarray sum < 0, then we can return the max value = 0, but if we have to return a non-empty set, then we have to return the best, whether it is positive or negative.
+    // * The only difference in both codes is the initialization of the best and current best variable.
+
+    // TODO For empty set - We will initialize both the variables from 0, so that we can ignore the updation of a negative best value.
+    // TODO For non empty set - We will initialize both the variables with INT_MIN, so that, we can have a negative max subarray sum.
+    int n, curr_best = 0, best = 0;
+    cin >> n;
+    vi v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    for(int i : v){
+        curr_best > 0 ? curr_best += i : curr_best = i;
+        if(best < curr_best)
+        best = curr_best;
+    }
+    cout << best << '\n';
+}
---
+++ Dynamic_programming/Longest_bitonic_sequence.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int b_search(vi &v, vi &dp, int left, int right, int target){
+    int midpoint = 0;
+    while (left < right){
+        midpoint = left + (right - left) / 2;
+        dp[midpoint] < target ? left = midpoint + 1 : right = midpoint;
+    }
+    if (left == dp.size() - 1){
+        if (dp[left] >= target)
+            dp[left] = target;
+
+        else
+            dp.push_back(target);
+    }
+    else{
+        if (dp[left] > target)
+            dp[left] = target;
+    }
+    return dp.size();
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie();
+
+    int n, left = 0, right = 0, max_len = 0;
+    cin >> n;
+    vi v(n), left_lis(n), right_lis(n), ldp, rdp;
+
+    for (int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    ldp.pb(v[0]);
+    rdp.pb(v[n - 1]);
+    left_lis[0] = right_lis[n - 1] = 1;
+
+
+    // * For finding the LIS from left side -
+
+    for(int i = 1; i < n; i++){
+        left = 0, right = ldp.size() - 1;
+        int lis_val = b_search(v, ldp, left, right, v[i]);
+        left_lis[i] = lis_val;
+    }
+    
+
+    // * For finding the LIS from right side -
+
+    for(int i = n - 1; i >= 0; i--){
+        left = 0, right = rdp.size() - 1;
+        int lis_val = b_search(v, rdp, left, right, v[i]);
+        right_lis[i] = lis_val;
+    }
+
+
+    // * For finding the maximum length of bitonic sequence
+
+    for(int i = 0; i < n; i++){
+        max_len = max(max_len, (left_lis[i] + right_lis[i]));
+    }
+
+
+    // * We will subtract 1 from the result because the index 'i' from where we got the maximum length of bitonic sequence, counted the index 'i' twice in the calculation of left LIS and right LIS because both times the index 'i' was included during the calculation.
+    cout << max_len - 1 << '\n';
+}
