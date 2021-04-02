14b899333e15beb42e67044302ca561a91bb9e66
Aditya Anand <anand.aditya0803@gmail.com>
2021-04-02 23:48:13 +0530

    Adding codes for DP

---
+++ Dynamic_programming/Minimum_deletions_to_make_string_palindrome.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    int n = (int)s.size();
+    vector<vector<int> > dp(n, vector<int>(n, 0));
+    for (int g = 0; g < n; g++)
+    {
+        for (int i = 0, j = g; j < n; i++, j++)
+        {
+            if (g == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else if (g == 1)
+            {
+                s[i] == s[j] ? dp[i][j] = 2 : dp[i][j] = 1;
+            }
+            else
+            {
+                s[i] == s[j] ? dp[i][j] = dp[i + 1][j - 1] + 2 : dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+            }
+        }
+    }
+    cout << n - dp[0][n - 1] << '\n';
+}
