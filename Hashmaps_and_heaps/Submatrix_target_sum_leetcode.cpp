f36ba4d32a46a6f4ad50634c0aa2eea8b84f4cbe
Aditya Anand <anand.aditya0803@gmail.com>
2021-04-18 23:50:19 +0530

    Adding codes for Hashmaps and Heaps

---
+++ Hashmaps_and_heaps/Submatrix_target_sum_leetcode.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int count_submatrices(vvi &matrix, int target){
+    int n = (int) matrix.size(), m = (int) matrix[0].size(), count = 0;
+    vvi prefix_sum(n, vi(m, 0));
+    for(int i = 0; i < n; i++){
+        prefix_sum[i][0] = matrix[i][0];
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j < m; j++){
+            prefix_sum[i][j] = prefix_sum[i][j - 1] + matrix[i][j];
+        }
+    }
+    
+    for(int k = 0; k < m; k++){
+        for(int j = k; j < m; j++){
+            unordered_map<int, int> my_map;
+            int matrix_sum = 0;
+            for(int i = 0; i < n; i++){
+                if(k == 0){
+                    prefix_sum[i][j] == target ? count++ : count = count;
+                    matrix_sum += prefix_sum[i][j];
+                    if(my_map.count(matrix_sum - k) > 0){
+                        count += my_map[matrix_sum - k];
+                    }
+                    my_map[matrix_sum] += 1;
+                }
+                else{
+                    prefix_sum[i][j] - prefix_sum[i][k - 1] == target ? count++ : count = count;
+                    matrix_sum += prefix_sum[i][j] - prefix_sum[i][k - 1];
+                    if(my_map.count(matrix_sum - k) > 0){
+                        count += my_map[matrix_sum - k];
+                    }
+                    my_map[matrix_sum] += 1;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, target;
+    cin >> n >> m;
+    vvi matrix(n, vi(m));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    cin >> target;
+    int count = count_submatrices(matrix, target);
+    cout << count << '\n';
+}
