4413219e67c636415e66d66d3df4a1d22e4c858e
Aditya Anand <anand.aditya0803@gmail.com>
2021-05-14 17:30:44 +0530

    Adding codes for DP

---
+++ Dynamic_programming/Counts.bracket.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vi dp(n + 1, 0);
+    dp[0] = dp[1] = 1;
+    for(int i  = 2; i <= n; i++)
+        for(int j = 0, k = i - 1; j < i; j++, k--)
+            dp[i] += dp[j] * dp[k];
+
+    cout << dp[n] << '\n';
+}
