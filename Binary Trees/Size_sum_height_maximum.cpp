dae992a4126ae5d2c18e0e6da3d9a0e8e983ba32
Aditya Anand <anand.aditya0803@gmail.com>
2021-06-01 02:18:29 +0530

    Adding codes for Binary Trees

---
+++ Binary Trees/Size_sum_height_maximum.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <utility>
+#include<string>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define INFI 10e8
+#define INF 10e7
+#define mod 1000000007
+#define sieve_limit 10e6
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<long long int> vlli;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<long long> > vvll;
+typedef vector<vector<long long int> > vvlli;
+typedef vector<vector<bool> > vvb;
+
+class Node{
+public:
+
+    int data;
+    Node *left, *right;
+    Node(int data){
+        this-> data = data;
+        this-> left = NULL;
+        this-> right = NULL;
+    }
+};
+
+Node* input(){
+    int size; 
+    cin >> size;
+    vector<string> v(size);
+    for(int i = 0; i < size; i++){
+        cin >> v[i];
+    }
+
+    int root_data = stoi(v[0]);
+    Node *root = new Node(root_data);
+    stack<pair<Node*, int> > my_stack;
+    my_stack.push(mp(root, 0));
+    int index = 0;
+    while(!my_stack.empty()){
+        auto top = my_stack.top();
+        my_stack.pop();
+        if(top.second == 0 && index < size){
+            index++;
+            top.second++;
+            my_stack.push(top);
+            if(v[index] == "n")
+                top.first-> left = NULL;
+            else{
+                int val = stoi(v[index]);
+                Node *left_child = new Node(val);
+                top.first-> left = left_child;
+                my_stack.push(mp(left_child, 0));
+            }      
+        }
+        else if(top.second == 1 && index < size){
+            index++;
+            top.second++;
+            my_stack.push(top);
+            if(v[index] == "n")
+                top.first-> right = NULL;
+            else{
+                int val = stoi(v[index]);
+                Node *right_child = new Node(val);
+                top.first-> right = right_child;
+                my_stack.push(mp(right_child, 0));
+            }  
+        }
+    }
+    return root;
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Node *root = input();   
+}
